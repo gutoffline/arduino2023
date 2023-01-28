c66c2a464041501c0810c64bb5eb2665862fdf56
Guto Xavier <gutoffline@gmail.com>
2023-01-28 10:31:38 -0300

    atividade 001

---
+++ aula002/001.c
@@ -0,0 +1,16 @@
+/* 
+Piscar um Led
+Esse é nosso primeiro código do dia 
+*/
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
---
+++ aula002/README.md
@@ -0,0 +1,14 @@
+# Aula02
+- //: comentário de uma linha
+- /* */: comentário de várias linhas.
+```c
+// esse é um comentário de uma linha
+
+/*
+esse é um comentário de 
+várias 
+linhas
+*/
+```
+
+
